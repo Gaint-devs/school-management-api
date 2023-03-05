e74ea0b00929c242f3cce98f1207b2ca23578981
parthobepary <pallobbiswas326@gmail.com>
2023-03-05 21:06:54 +0600

    authentication done

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // setup
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv'
 import postRouter from './routes/post.js';
-dotenv.config();
+import authRouter from './routes/auth.js';
 const port = process.env.port || 5000
 
 const app = express();
 // middleware 
 app.use(cors());
 app.use(express.json());
-app.use(postRouter);
+app.use(postRouter, authRouter);
 
 
 //api route
